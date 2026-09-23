33c70d7eef6007d1c07f3c5413d6249b75dab32e
Jaylowkey <jrsamadh@gmail.com>
2026-09-23 15:56:11 +0200

    set login API to node runtime

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { createSession, verifyPassword } from "@/lib/auth";
+export const runtime = "nodejs";
 
 export async function POST(request: Request) {
   try {
